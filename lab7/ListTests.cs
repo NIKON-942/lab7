09e4c4328e4c542feddafc95ef27095197d41035
Mykyta <gubin.mykyta@lll.kpi.ua>
2025-03-12 21:49:42 +0200

    first commit

---
+++ lab7/ListTests.cs
@@ -0,0 +1,256 @@
+﻿namespace lab7;
+
+public static class ListTests
+{
+    public static void Main()
+    {
+        ConsoleKey key;
+        string[] menuOptions = ["TestCreateEmptyList", "TestCreatingListFromFloats", "TestAddingFloats", "TestAddingNodes", "TestDeletingByValue", "TestDeletingByNode", "TestContainsElement", "TestToArray", "TestToList", "TestClone", "TestGreaterThan", "TestSumOfElementsLessThanFirstNegative", "NewListGreaterThan", "TestDeleteElementsLessThan"];
+        var selectedOption = 0;
+        
+        do
+        {
+            Console.Clear();
+            for (var i = 0; i < menuOptions.Length; i++)
+            {
+                if (i == selectedOption)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine(i + 1 + "." + menuOptions[i]);
+                Console.ResetColor();
+            }
+        
+            key = Console.ReadKey().Key;
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    selectedOption = Math.Max(0, selectedOption - 1);
+                    break;
+                case ConsoleKey.DownArrow:
+                    selectedOption = Math.Min(menuOptions.Length - 1, selectedOption + 1);
+                    break;
+                case ConsoleKey.Enter:
+                    Console.Clear();
+                    Handle(selectedOption);
+                    key = Console.ReadKey().Key;
+                    break;
+            }
+        } while (key != ConsoleKey.Escape);
+        Console.Clear();
+    }
+
+    private static void Handle(int option)
+    {
+        switch (option)
+        {
+            case 0:
+                TestCreateEmptyList();
+                break;
+            case 1:
+                TestCreatingListFromFloats();
+                break;
+            case 2:
+                TestAddingFloats();
+                break;
+            case 3:
+                TestAddingNodes();
+                break;
+            case 4:
+                TestDeletingByValue();
+                break;
+            case 5:
+                TestDeletingByNode();
+                break;
+            case 6:
+                TestContainsElement();
+                break;
+            case 7:
+                TestToArray();
+                break;
+            case 8:
+                TestToList();
+                break;
+            case 9:
+                TestClone();
+                break;
+            case 10:
+                TestGreaterThan();
+                break;
+            case 11:
+                TestSumOfElementsLessThanFirstNegative();
+                break;
+            case 12:
+                NewListGreaterThan();
+                break;
+            case 13:
+                TestDeleteElementsLessThan();
+                break;
+        }
+    }
+    
+    private static void TestCreateEmptyList()
+    {
+        Console.WriteLine("Test_1");
+        var emptyList = new SinglyLinkedListNode();
+        Console.WriteLine($"Empty list was created. List: {emptyList}. Count of list values: {emptyList.Count()}");
+    }
+    
+    private static void TestCreatingListFromFloats()
+    {
+        Console.WriteLine("Test_2");
+        Console.WriteLine("Creating list from values: 12.3, 13.4, -41.88, 0, 45");
+        var list1 = new SinglyLinkedListNode(12.3f, 13.4f, -41.88f, 0, 45f);
+        Console.WriteLine($"Created list: {list1}");
+        Console.WriteLine("Creating list from values: 13e-4, -3, 0.52, -1.55, 1.45e3");
+        var list2 = new SinglyLinkedListNode(13e-4f, -3f, 0.88f, -1.55f, 1.45e3f);
+        Console.WriteLine($"Created list: {list2}");
+    }
+
+    private static void TestAddingFloats()
+    {
+        Console.WriteLine("Test_3");
+        var list = new SinglyLinkedListNode();
+        Console.WriteLine($"Creating empty list. List: {list}");
+        list.AddNodes(2.5f, 77f, 1e-4f, 5f);
+        Console.WriteLine($"Adding values 2.5, 77, 1e-4, 5. List: {list}");
+        list.AddNodes(77f, 4e5f, 21f, 3e-6f);
+        Console.WriteLine($"Adding values 77, 4e5, 21, 3e-6. List: {list}");
+    }
+    
+    private static void TestAddingNodes()
+    {
+        Console.WriteLine("Test_4");
+        var list = new SinglyLinkedListNode(78f, 16.44f);
+        Console.WriteLine($"Creating new list with values 78, 16.44. List: {list}");
+        var node1 = new SinglyLinkedListNode(50f);
+        var node2 = new SinglyLinkedListNode(-2f, 0.33f, 54e-1f);
+        Console.WriteLine($"Adding nodes to list: {node1}, {node2}");
+        list.AddNodes(node1);
+        list.AddNodes(node2);
+        Console.WriteLine($"List after adding nodes: {list}");
+    }
+    
+    private static void TestDeletingByValue()
+    {
+        Console.WriteLine("Test_5");
+        var list = new SinglyLinkedListNode(44f, 10.33f, -4.32f, -32f);
+        Console.WriteLine($"Creating new list with values 44, 10.33, -4.32, 32. List: {list}");
+        list = list.DeleteNode(44f);
+        Console.WriteLine($"Deleting value 44. List: {list}");
+        list = list.DeleteNode(4f);
+        Console.WriteLine($"Deleting value 4. List: {list}");
+    }
+
+    public static void TestDeletingByNode()
+    {
+        Console.WriteLine("Test_6");
+        var node1 = new SinglyLinkedListNode(66.2f);
+        var node2 = new SinglyLinkedListNode(-3.12f);
+        var node3 = new SinglyLinkedListNode(92.134f);
+        var node4 = new SinglyLinkedListNode(52f);
+        Console.WriteLine($"Creating empty list and adding nodes: {node1}, {node2}, {node3}");
+        var list = new SinglyLinkedListNode();
+        list.AddNodes(node1, node2, node3);
+        list = list.DeleteNode(node2);
+        Console.WriteLine($"Trying to delete node {node2}: {list}");
+        list = list.DeleteNode(node4);
+        Console.WriteLine($"Trying to delete {node4}: {list}");
+    }
+    
+    private static void TestContainsElement()
+    {
+        Console.WriteLine("Test_7");
+        var list = new SinglyLinkedListNode(12f, -6.63f, 8.88f, 12f, -32f);
+        Console.WriteLine($"Creating new list with values 12, -6.63, 8.88, 12, -32. List: {list}");
+        var check1 = list.Contains(12f);
+        var check2 = list.Contains(5f);
+        var check3 = list.Contains(8.88f);
+        Console.WriteLine("Checking if list contains:");
+        Console.WriteLine("--> 12   - " + check1);
+        Console.WriteLine("--> 5    - " + check2);
+        Console.WriteLine("--> 8.88 - " + check3);
+    }
+    
+    private static void TestToArray()
+    {
+        Console.WriteLine("Test_8");
+        var list = new SinglyLinkedListNode(1e3f, -55f, 3.21f, 0);
+        Console.WriteLine($"Creating new list with values 1e3, -55, 3.21, 0. List: {list}");
+        Console.WriteLine($"Type of current list: {list.GetType()}");
+        var array = list.ToArray();
+        Console.WriteLine($"Array from list: {array.ToArray().Aggregate("", (s, f) => s + f + ", ")}");
+        Console.WriteLine($"Type of current array {array.GetType()}");
+    }
+    
+    private static void TestToList()
+    {
+        Console.WriteLine("Test_9");
+        var singlyLinkedList = new SinglyLinkedListNode(1e3f, -55f, 3.21f, 0);
+        Console.WriteLine($"Creating new list with values 1e3, -55, 3.21, 0. List: {singlyLinkedList}");
+        Console.WriteLine($"Type of current list: {singlyLinkedList.GetType()}");
+        var list = singlyLinkedList.ToList();
+        Console.WriteLine($"Array from list: {list.Aggregate("", (s, f) => s + f + ", ")}");
+        Console.WriteLine($"Type of current array {list.GetType()}");
+    }
+    
+    private static void TestClone()
+    {
+        Console.WriteLine("Test_10");
+        var list = new SinglyLinkedListNode(4e-3f, 14f, -4.90f, 0.34f);
+        Console.WriteLine($"Creating new list with values 4e-3, 14, -4.90, 0.34. List: {list}");
+        var cloned = (SinglyLinkedListNode)list.Clone();
+        Console.WriteLine($"Cloning list: {cloned}");
+        Console.WriteLine($"Checking if list == cloned: {list == cloned}");
+    }
+
+    private static void TestGreaterThan()
+    {
+        Console.WriteLine("Test_11");
+        var list = new SinglyLinkedListNode(55f, -3.52f, 99.08f, 2f);
+        Console.WriteLine($"Creating new list with values 55, -3.52, 99.08, 2. List: {list}");
+        Console.WriteLine($"First bigger than 60 is {list.FirstGreaterThan(60)}");
+        Console.WriteLine($"First bigger than -5 is {list.FirstGreaterThan(-5)}");
+        var noNumber = list.FirstGreaterThan(100);
+        Console.WriteLine(noNumber.HasValue
+            ? $"First greater than 100 is {noNumber}"
+            : "List don't have values greater than 100");
+    }
+    
+    private static void TestSumOfElementsLessThanFirstNegative()
+    {
+        Console.WriteLine("Test_12");
+        var list1 = new SinglyLinkedListNode(34f, -3f, -44.56f, 9f, -8f);
+        Console.WriteLine($"Creating new list with values 34, -3, -44.56, 9, -8. List: {list1}");
+        Console.WriteLine($"Sum of elements that less than first negative is {list1.SumOfElementsLessThanFirstNegative()}");
+        var list2 = new SinglyLinkedListNode(22.7f, -55f, -9f, 33.10f, 16f);
+        Console.WriteLine($"Creating new list with values 22.7, -55, -9, 33.10, 16. List: {list2}");
+        var sum2 = list2.SumOfElementsLessThanFirstNegative();
+        Console.WriteLine(sum2.HasValue
+            ? $"Sum of elements that less than first negative is {sum2}"
+            : "List don't have values that less than first negative or don't have negative elements");
+    }
+    
+    private static void NewListGreaterThan()
+    {
+        Console.WriteLine("Test_13");
+        var list = new SinglyLinkedListNode(55f, -3.52f, 99.08f, 2f);
+        Console.WriteLine($"Creating new list with values 55, -3.52, 99.08, 2. List: {list}");
+        var newList1 = list.NewListGreaterThan(1);
+        Console.WriteLine($"Creating new list with values greater than 1 : {newList1}");
+        var newList2 = list.NewListGreaterThan(100);
+        Console.WriteLine($"Creating new list with values greater than 60 : {newList2}");
+    }
+
+    private static void TestDeleteElementsLessThan()
+    {
+        Console.WriteLine("Test_14");
+        var list = new SinglyLinkedListNode(12f, -6.63f, 8.88f, 12f, -32f, 19f, 44f, -40f, -5.45f);
+        Console.WriteLine($"Creating new list with values 12, -6.63, 8.88, 12, -32, 19, 44, -40, -5.45. List: {list}");
+        var newList1 = list.DeleteElementsLessThan(5);
+        Console.WriteLine($"Deleting from list elements less than 5 : {newList1}");
+        var newList2 = list.DeleteElementsLessThan(40);
+        Console.WriteLine($"Deleting from list elements less than 40 : {newList2}");
+    }
+}
---
+++ lab7/SinglyLinkedList.cs
@@ -0,0 +1,287 @@
+﻿using System.Collections;
+using System.Globalization;
+
+namespace lab7;
+
+/// <summary>
+/// Represents a singly linked list that stores float values.
+/// Each node contains a float value and a reference to the next node.
+/// </summary>
+public class SinglyLinkedListNode : IEnumerable<SinglyLinkedListNode>, ICloneable
+{
+    public float? Value { get; set; }
+    public SinglyLinkedListNode? NextNode { get; private set; }
+
+    /// <summary>
+    /// Creates an empty singly linked list.
+    /// </summary>
+    public SinglyLinkedListNode()
+    {
+        Value = null;
+        NextNode = null;
+    }
+
+    /// <summary>
+    /// Creates singly linked list from values.
+    /// </summary>
+    /// <param name="values">List values</param>
+    public SinglyLinkedListNode(params float[] values) 
+    {
+        Value = values[0];
+        NextNode = values.Length > 1 
+            ? new SinglyLinkedListNode(values[1..]) 
+            : null;
+    }
+
+    /// <summary>
+    /// Adds new values to list from floats.
+    /// </summary>
+    /// <param name="values">Values to add to list</param>
+    public void AddNodes(params float[] values) 
+    {
+        if (Value == null)
+        {
+            Value = values[0];
+            NextNode = values.Length > 1 
+                ? new SinglyLinkedListNode(values[1..]) 
+                : null;
+            return;
+        }
+        
+        var tempNode = this;
+        while (tempNode.NextNode != null) 
+            tempNode = tempNode.NextNode;
+
+        tempNode.NextNode = new SinglyLinkedListNode(values);
+    }
+
+    /// <summary>
+    /// Adds new values to list from nodes.
+    /// </summary>
+    /// <param name="nodes">Nodes to add to list</param>
+    public void AddNodes(params SinglyLinkedListNode[] nodes)
+    {
+        if (Value == null)
+        {
+            Value = nodes[0].Value;
+            NextNode = nodes[0].NextNode;
+            if (nodes.Length > 1)
+                AddNodes(nodes[1..]);
+            return;
+        }
+        
+        var tempNode = this;
+        while (tempNode.NextNode != null) 
+            tempNode = tempNode.NextNode;
+        
+        foreach (var newNode in nodes)
+        {
+            tempNode.NextNode = newNode;
+            while (tempNode.NextNode != null)
+            {
+                if (tempNode.NextNode == this)
+                    tempNode.NextNode = null;
+                else
+                    tempNode = tempNode.NextNode;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Deletes a node from the list by value.
+    /// </summary>
+    /// <param name="value">Value to delete</param>
+    /// <returns>New head of singly linked list</returns>
+    public SinglyLinkedListNode DeleteNode(float value)
+    {
+        if (!Contains(value))
+            return this;
+        
+        if (Value == value && NextNode != null)
+            return NextNode;
+
+        if (Value == value)
+        {
+            Value = null;
+            NextNode = null;
+            return this;
+        }
+
+        var tempNode = this;
+        while (tempNode.NextNode != null && tempNode.NextNode.Value != value)
+            tempNode = tempNode.NextNode;
+
+        if (tempNode.NextNode == null)
+            return this;
+
+        tempNode.NextNode = tempNode.NextNode.NextNode;
+        return this;
+    }
+
+    /// <summary>
+    /// Deletes a specified node from the list.
+    /// </summary>
+    /// <param name="node">Node to delete</param>
+    /// <returns>New head of singly linked list</returns>
+    public SinglyLinkedListNode DeleteNode(SinglyLinkedListNode node)
+    {
+        if (this == node && NextNode != null)
+            return NextNode;
+    
+        if (this == node)
+        {
+            Value = null;
+            return this;
+        }
+        
+        var tempNode = this;
+        while (tempNode.NextNode != null && tempNode.NextNode != node)
+            tempNode = tempNode.NextNode;
+    
+        if (tempNode.NextNode == null)
+            return this;
+    
+        tempNode.NextNode = tempNode.NextNode.NextNode;
+        return this;
+    }
+    
+    /// <summary>
+    /// Checks if a list contains a specific value.
+    /// </summary>
+    /// <param name="value">Value to check</param>
+    /// <returns>True if list contains a passed element, otherwise false</returns>
+    public bool Contains(float value) => this.Any(n => n.Value == value);
+
+    /// <summary>
+    /// Returns count of elements in singly linked list.
+    /// </summary>
+    /// <returns>Count of elements</returns>
+    public int Count() => this.Count(n => n.Value.HasValue);
+    
+    /// <summary>
+    /// Returns a list of singly linked list values.
+    /// </summary>
+    /// <returns>List of values</returns>
+    public List<float> ToList() => this.Where(n => n.Value.HasValue)
+            .Select(n => n.Value!.Value)
+            .ToList();
+
+    /// <summary>
+    /// Returns an array of singly linked list values.
+    /// </summary>
+    /// <returns>Array of values</returns>
+    public float[] ToArray() => this.Where(n => n.Value.HasValue)
+            .Select(n => n.Value!.Value)
+            .ToArray();
+    
+    /// <summary>
+    /// Returns a string that contains all list elements.
+    /// </summary>
+    /// <returns>List converted to string</returns>
+    public override string ToString() => "[" + 
+                                         this.Select(n => Convert.ToString(n.Value, CultureInfo.InvariantCulture))
+                                             .Aggregate((a, b) => a + ", " + b) + 
+                                         "]";
+
+    /// <summary>
+    /// Returns clone of that singly linked list.
+    /// </summary>
+    /// <returns>Object of cloned linked list</returns>
+    public object Clone() => new SinglyLinkedListNode(ToArray());
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public IEnumerator<SinglyLinkedListNode> GetEnumerator() => new SinglyLinkedListNodeEnum(this);
+}
+
+internal class SinglyLinkedListNodeEnum : IEnumerator<SinglyLinkedListNode>
+{
+    private readonly SinglyLinkedListNode _startNode;
+    private SinglyLinkedListNode? _current;
+
+    public SinglyLinkedListNodeEnum(SinglyLinkedListNode startNode) => (_startNode, _current) = (startNode, null);
+
+    public bool MoveNext()
+    {
+        if (_current == null)
+        {
+            _current = _startNode;
+            return true;
+        }
+
+        if (_current.NextNode == null)
+            return false;
+        
+        _current = _current.NextNode;
+        return true;
+    }
+
+    public void Reset() => _current = null;
+
+    object? IEnumerator.Current => _current;
+
+    public void Dispose() { }
+
+    SinglyLinkedListNode IEnumerator<SinglyLinkedListNode>.Current => _current;
+}
+
+public static class Tasks
+{
+    /// <summary>
+    /// Finds the first value in the list greater than the passed value.
+    /// </summary>
+    /// <param name="node">Node to find value</param>
+    /// <param name="value">Value to find greater than</param>
+    /// <returns>First value greater than passed or null if all values less than passed.</returns>
+    public static float? FirstGreaterThan(this SinglyLinkedListNode node, float value) =>
+        node.FirstOrDefault(n => n.Value > value)?.Value;
+
+    /// <summary>
+    /// Calculate sum of elements that less than first negative element of a list.
+    /// </summary>
+    /// <param name="node">Source of elements</param>
+    /// <returns>Sum of elements that less than first negative or null if list doesn't have negative elements or all elements are greater than first negative element.</returns>
+    public static float? SumOfElementsLessThanFirstNegative(this SinglyLinkedListNode node)
+    {
+        var firstNegative = node.FirstOrDefault(n => n.Value < 0)?.Value;
+        if (node.FirstOrDefault(n => n.Value < 0)?.Value == null)
+            return null;
+        return node.Any(n => n.Value < firstNegative) 
+            ? node.Where(n => n.Value < firstNegative)
+                .Select(n => n.Value)
+                .Sum() 
+            : null;
+    }
+
+    /// <summary>
+    /// Creates new list with values that passed value.
+    /// </summary>
+    /// <param name="node">Source of elements</param>
+    /// <param name="value">Value to find values greater than</param>
+    /// <returns>New singly linked list with values greater than the passed one, if any, otherwise null.</returns>
+    public static SinglyLinkedListNode? NewListGreaterThan(this SinglyLinkedListNode node, float value)
+    {
+        return node.Any(n => n.Value > value) 
+            ? new SinglyLinkedListNode(node.Where(n => n.Value > value && n.Value.HasValue)
+                .Select(n => n.Value!.Value)
+                .ToArray()) 
+            : null;
+    }
+    
+    /// <summary>
+    /// Deletes from singly linked list values less than the passed one.
+    /// </summary>
+    /// <param name="node">Source of elements</param>
+    /// <param name="value">Value to delete elements less than</param>
+    /// <returns></returns>
+    public static SinglyLinkedListNode DeleteElementsLessThan(this SinglyLinkedListNode node, float value)
+    {
+        if (!node.Any(n => n.Value < value))
+            return node;
+        
+        var nodeToReturn = node;
+        node.Where(n => n.Value < value).ToList()
+            .ForEach(n => nodeToReturn = nodeToReturn.DeleteNode(n.Value.Value));
+        return nodeToReturn;
+    }
+}
